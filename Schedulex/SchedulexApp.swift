19db5e301c3630dccc15236582b337ef00b77ea9
Sebastian Staszczyk <sebastian.staszczyk@finanteq.com>
2023-07-18 20:25:40 +0200

    Initial Commit

---
+++ Schedulex/SchedulexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchedulexApp.swift
+//  Schedulex
+//
+//  Created by Sebastian Staszczyk on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SchedulexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
