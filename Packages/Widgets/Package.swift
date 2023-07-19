263af4602d220c6cb2214e6bd87c1f57a629491f
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-07-19 12:40:32 +0200

    Project setup

---
+++ Packages/Widgets/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Widgets",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Widgets",
+            targets: ["Widgets"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Widgets"),
+        .testTarget(
+            name: "WidgetsTests",
+            dependencies: ["Widgets"]),
+    ]
+)
