73974104d92b388e1a804afbe749a694505aec6c
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-08-07 18:44:23 +0200

    Adjust event date

--- Packages/Domain/Sources/Domain/Event.swift
+++ Packages/Domain/Sources/Domain/Event.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public struct Event: Hashable, Decodable {
-    public let time: String
-    public let date: String
-    public let name: String
+    public let startDate: Date?
+    public let endDate: Date?
+    public let name: String?
     public let place: String?
-    public let teacher: String
-    public let type: String
+    public let teacher: String?
+    public let type: String?
 }
