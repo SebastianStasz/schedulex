43938982e467014c6a988a57f3d067e5f4fd1b71
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-09-21 16:43:56 +0200

    Design system: Text button

---
+++ Packages/Widgets/Sources/Widgets/Buttons/TextButton.swift
@@ -0,0 +1,35 @@
+//
+//  TextButton.swift
+//  Widgets
+//
+//  Created by Sebastian Staszczyk on 21/09/2023.
+//
+
+import SwiftUI
+
+private struct TextButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundStyle(.accentPrimary)
+            .opacity(configuration.isPressed ? 0.6 : 1)
+    }
+}
+
+public struct TextButton: View {
+    private let title: String
+    private let action: () -> Void
+
+    public init(_ title: String, action: @escaping () -> Void) {
+        self.title = title
+        self.action = action
+    }
+
+    public var body: some View {
+        Button(title, action: action)
+            .buttonStyle(TextButtonStyle())
+    }
+}
+
+#Preview {
+    TextButton("Button", action: {})
+}
