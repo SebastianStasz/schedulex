dbd4e8ee09e319c2c794ce946dc5b860816c54fd
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-09-22 19:07:11 +0200

    Back button appearance

--- Schedulex/App/AppDelegate.swift
+++ Schedulex/App/AppDelegate.swift
@@ -7,11 +7,13 @@
 
 import Firebase
 import UIKit
+import Widgets
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        configureBackButtonAppearance()
         return true
     }
 
@@ -27,5 +29,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+    private func configureBackButtonAppearance() {
+        var config = UIImage.SymbolConfiguration(paletteColors: [.textPrimary])
+        config = config.applying(UIImage.SymbolConfiguration(font: .systemFont(ofSize: 18, weight: .medium)))
+
+        let backButtonImage = UIImage(systemName: "arrow.backward", withConfiguration: config)?
+            .withAlignmentRectInsets(UIEdgeInsets(top: 0, left: -4, bottom: 0, right: 0))
+
+        UINavigationBar.appearance().backIndicatorImage = backButtonImage
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = backButtonImage
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: .normal)
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear], for: UIControl.State.highlighted)
     }
 }
