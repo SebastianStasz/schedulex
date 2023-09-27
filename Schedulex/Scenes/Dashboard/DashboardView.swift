615de88dbb9a54161b28b79c9438bbcfe817b124
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-09-27 20:40:15 +0200

    Dashboard view improvements

--- Schedulex/Scenes/Dashboard/DashboardView.swift
+++ Schedulex/Scenes/Dashboard/DashboardView.swift
@@ -5,18 +5,96 @@
 //  Created by Sebastian Staszczyk on 31/07/2023.
 //
 
+import Domain
 import SwiftUI
+import Widgets
+import SchedulexFirebase
 
-struct DashboardView: RootView {
-    @ObservedObject var store: DashboardStore
+struct DashboardView: View {
+    @AppStorage("subscribedFacultyGroups") private var subscribedGroups: [FacultyGroup] = []
+    @StateObject private var viewModel = DashboardViewModel()
+    @State private var service = FirestoreService()
+    @State private var isDatePickerPresented = false
+    @State private var isSchedulesSheetPresented = false
 
-    var rootBody: some View {
-        Text("Dashboard")
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 0) {
+                if let startDate = viewModel.startDate, let endDate = viewModel.endDate {
+                    LazyVStack {
+                        DayPickerView(startDate: startDate, endDate: endDate, shouldScrollToDay: isDatePickerPresented, selection: $viewModel.selectedDate)
+                    }
+                    .padding(.top, .xlarge)
+                    .padding(.bottom, .medium)
+                    .background(.backgroundSecondary)
+
+                    Rectangle()
+                        .fill(.grayShade1)
+                        .frame(height: 1)
+                        .opacity(0.15)
+
+                    ScrollView {
+                        VStack(spacing: .medium) {
+                            ForEach(viewModel.selectedDateEvents, id: \.self) { event in
+                                EventCardView(event: event)
+                            }
+                        }
+                        .padding(.top, .large)
+                        .padding(.horizontal, .medium)
+                        .background(.backgroundPrimary)
+                    }
+                }
+            }
+            .overlay {
+                if viewModel.selectedDateEvents.isEmpty {
+                    Text("No events here", style: .body)
+                        .foregroundStyle(.grayShade1)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .bottomBar) {
+                    HStack(spacing: 0) {
+                        TextButton("Schedules") { isSchedulesSheetPresented = true }
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        TextButton("Today") { viewModel.selectedDate = .now }
+                            .frame(maxWidth: .infinity)
+                        TextButton("Calendar") { isDatePickerPresented = true }
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    .padding(.horizontal, .micro)
+                }
+            }
+            .sheet(isPresented: $isDatePickerPresented) { datePicker }
+            .sheet(isPresented: $isSchedulesSheetPresented) { ObservedFacultyGroupsView(service: service) }
+        }
+
+        .doubleNavigationTitle(title: viewModel.title, subtitle: viewModel.subtitle)
+        .toolbar(.hidden)
+        .task {
+            Task {
+                try await viewModel.fetchEvents(for: subscribedGroups)
+            }
+        }
+//        .onChange(of: subscribedGroups) {
+//            viewModel.fetchEvents(for: $0)
+//        }
+    }
+
+    @ViewBuilder
+    private var datePicker: some View {
+        if let startDate = viewModel.startDate, let endDate = viewModel.endDate {
+            DatePicker("Select date", selection: $viewModel.selectedDate, in: startDate...endDate, displayedComponents: .date)
+                .datePickerStyle(.graphical)
+                .padding(.horizontal, .medium)
+                .presentationDetents([.height(380)])
+                .presentationDragIndicator(.visible)
+                .tint(Color.accentPrimary)
+        }
     }
 }
 
 struct DashboardView_Previews: PreviewProvider {
     static var previews: some View {
-        DashboardView(store: DashboardStore())
+        DashboardView()
     }
 }
--- Schedulex/Scenes/Dashboard/DashboardViewModel.swift
+++ Schedulex/Scenes/Dashboard/DashboardViewModel.swift
@@ -5,12 +5,71 @@
 //  Created by Sebastian Staszczyk on 30/08/2023.
 //
 
+import Combine
+import Domain
 import Foundation
-import SchedulexFirebase
+import UEKScraper
+import Widgets
 
-struct DashboardViewModel: ViewModel {
-    func makeStore(storage: FirestoreService) -> DashboardStore {
-        let store = DashboardStore()
-        return store
+@MainActor
+final class DashboardViewModel: ObservableObject {
+    @Published private(set) var selectedDateEvents: [Event] = []
+    @Published private(set) var startDate: Date?
+    @Published private(set) var endDate: Date?
+
+    @Published private var allEvents: [Event] = []
+    @Published var selectedDate: Date = .now
+
+    private let service = UekScheduleService()
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() { bind() }
+
+    var title: String {
+        selectedDate.formatted(style: .dateLong)
+    }
+
+    var subtitle: String {
+        selectedDate.isSameDay(as: .now) ? "Today" : "Selected date"
+    }
+
+    func fetchEvents(for facultyGroups: [FacultyGroup]) async throws {
+        var events: [Event] = []
+        for facultyGroup in facultyGroups {
+            let facultyGroupEvents = try await service.getFacultyGroupEvents(for: facultyGroup)
+            events.append(contentsOf: facultyGroupEvents.events)
+        }
+        allEvents = events
+    }
+
+    func bind() {
+        $allEvents
+            .map { $0.sorted(by: { $0.startDate! < $1.startDate! }) }
+            .sink { [weak self] in
+                self?.startDate = $0.first?.startDate
+                self?.endDate = $0.last?.startDate
+            }
+            .store(in: &cancellables)
+
+        CombineLatest($startDate.compactMap { $0 }, $endDate.compactMap { $0 })
+            .map { startDate, endDate in
+                let todayDate = Date.now
+                if todayDate < startDate {
+                    return startDate
+                } else if todayDate > endDate {
+                    return endDate
+                } else {
+                    return todayDate
+                }
+            }
+            .assign(to: &$selectedDate)
+
+        CombineLatest($allEvents, $selectedDate)
+            .map { allEvents, selectedDate in
+                allEvents
+                    .filter { $0.startDate?.formatted(date: .numeric, time: .omitted) == selectedDate.formatted(date: .numeric, time: .omitted) }
+                    .sorted(by: { $0.startDate! < $1.startDate! })
+            }
+            .assign(to: &$selectedDateEvents)
     }
 }
