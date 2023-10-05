85dec3c2c66075f8c6f9f4beb2ebee6efcaf8209
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-10-05 17:33:03 +0200

    FirestoreService fetch data again after one hour

--- Packages/SchedulexFirebase/Sources/SchedulexFirebase/FirestoreService.swift
+++ Packages/SchedulexFirebase/Sources/SchedulexFirebase/FirestoreService.swift
@@ -11,11 +11,15 @@ import FirebaseFirestoreSwift
 
 final public class FirestoreService {
     private lazy var db = Firestore.firestore()
+    private var lastUpdateDate: Date = .now
     private var school: School?
 
     public init() {}
 
     public func getCracowUniversityOfEconomics() async throws -> School {
+        guard shouldUseCachedData else {
+            return try await fetchCracowUniversityOfEconomics()
+        }
         if let school {
             return school
         }
@@ -25,7 +29,16 @@ final public class FirestoreService {
     private func fetchCracowUniversityOfEconomics() async throws -> School {
         let document = try await db.collection("schools").document("cracow_university_of_economics").getDocument()
         let school = try! document.data(as: School.self)
+        lastUpdateDate = .now
         self.school = school
         return school
+    }
+
+    private var shouldUseCachedData: Bool {
+        let fetchAgainDate = Calendar.current.date(byAdding: .hour, value: 1, to: lastUpdateDate)
+        guard let fetchAgainDate, fetchAgainDate >= Date.now else {
+            return false
+        }
+        return true
     }
 }
