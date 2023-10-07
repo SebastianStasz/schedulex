be468e6f11ca97a58a2e274dd7c648d3597e590d
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-10-07 10:53:47 +0200

    Change default localization to en

--- Packages/Resources/Package.swift
+++ Packages/Resources/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Resources",
-    defaultLocalization: "pl", 
+    defaultLocalization: "en", 
     platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
