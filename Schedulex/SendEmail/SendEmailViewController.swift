5a4f116747d8b3d12febe4a66bbf1ba95dbdcfd9
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2024-03-06 17:03:34 +0100

    Fix missing properties in send email view controller

--- Schedulex/SendEmail/SendEmailViewController.swift
+++ Schedulex/SendEmail/SendEmailViewController.swift
@@ -9,19 +9,17 @@ import MessageUI
 import SwiftUI
 
 final class SendEmailViewController: MFMailComposeViewController, MFMailComposeViewControllerDelegate {
-    private let emailContent: EmailContent
-
     init(emailContent: EmailContent) {
-        self.emailContent = emailContent
         super.init(nibName: nil, bundle: nil)
+        
+        setToRecipients([emailContent.recipient])
+        setSubject(emailContent.subject)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mailComposeDelegate = self
-        setToRecipients([emailContent.recipient])
-        setSubject(emailContent.subject)
     }
 
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
