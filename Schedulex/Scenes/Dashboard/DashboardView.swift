eb52a0af6681057c3c6f29019c6b9565949a2d13
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2024-06-11 18:47:47 +0200

    Pavilions list: Dashboard title

--- Schedulex/Scenes/Dashboard/DashboardView.swift
+++ Schedulex/Scenes/Dashboard/DashboardView.swift
@@ -182,6 +182,11 @@ struct DashboardView: RootView {
 }
 
 final class DashboardViewController: SwiftUIViewController<DashboardViewModel, DashboardView> {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = L10n.dashboardTitle
+    }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
