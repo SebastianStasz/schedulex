f07176937013ba323b83c3267ecb776e12f1ec89
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2024-10-03 20:27:39 +0200

    Campus map: Refactor view

--- Schedulex/Scenes/CampusMapView.swift
+++ Schedulex/Scenes/CampusMapView.swift
@@ -14,36 +14,22 @@ import Widgets
 
 @available(iOS 17.0, *)
 struct CampusMapView: RootView {
-    var store: CampusMapStore
+    @ObservedObject var store: CampusMapStore
 
     init(store: CampusMapStore) {
         self.store = store
     }
 
-    @State private var position: MapCameraPosition = .camera(mapCamera)
-    @State private var visibleRegion: MKCoordinateRegion?
-    @State private var selectedBuilding: UekBuilding?
-
     var rootBody: some View {
-        Map(position: $position, selection: $selectedBuilding) {
+        Map(position: $store.position, selection: $store.selectedBuilding) {
             ForEach(UekBuilding.allCases, content: makeMarker)
         }
         .mapStyle(.standard(pointsOfInterest: .including([.cafe, .restaurant, .parking])))
         .mapControls {
             MapPitchToggle()
         }
-        .onMapCameraChange { visibleRegion = $0.region }
-        .safeAreaInset(edge: .bottom) {
-            HStack(spacing: 0) {
-                Spacer()
-                ResetExpandableButton(title: L10n.campusMapReset, isDefaultValueSelected: !isRegionDifferent, action: {
-                    position = .camera(Self.mapCamera)
-                })
-            }
-            .padding(.top, .small)
-            .padding(.horizontal, .large)
-            .background(.thinMaterial)
-        }
+        .onMapCameraChange { store.visibleRegion = $0.region }
+        .safeAreaInset(edge: .bottom, content: makeBottomMenu)
     }
 
     private func makeMarker(for building: UekBuilding) -> some MapContent {
@@ -52,22 +38,16 @@ struct CampusMapView: RootView {
             .tag(building)
     }
 
-    private var isRegionDifferent: Bool {
-        guard let currentLatitude = visibleRegion?.center.latitude,
-              let currentLongitude = visibleRegion?.center.longitude
-        else { return true }
-
-        let tolerance: CLLocationDegrees = 0.002
-        return abs(currentLatitude - Self.defaultCoordinate.latitude) > tolerance ||
-            abs(currentLongitude - Self.defaultCoordinate.longitude) > tolerance
-    }
-
-    private static var mapCamera: MapCamera {
-        MapCamera(centerCoordinate: defaultCoordinate, distance: 980, heading: 110, pitch: 30)
-    }
-
-    private static var defaultCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: 50.068638, longitude: 19.954687)
+    private func makeBottomMenu() -> some View {
+        HStack(spacing: 0) {
+            Spacer()
+            ResetExpandableButton(title: L10n.campusMapReset, isDefaultValueSelected: store.isDefaultRegionVisible) {
+                store.resetPosition.send()
+            }
+        }
+        .padding(.top, .small)
+        .padding(.horizontal, .large)
+        .background(.thinMaterial)
     }
 }
 
