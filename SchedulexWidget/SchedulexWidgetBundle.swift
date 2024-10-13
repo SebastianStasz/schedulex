760ad8a86bb3d1d74e312778e4de03032362a3c9
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2024-10-13 18:42:09 +0200

    Widgets: Setup target

---
+++ SchedulexWidget/SchedulexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  SchedulexWidgetBundle.swift
+//  SchedulexWidget
+//
+//  Created by Sebastian Staszczyk on 10/10/2024.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct SchedulexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        ScheduleWidgetSmall()
+    }
+}
